42fb62921d7935a8a30b2a7d9d1bae7140d3ecd8
Naser Kourosh <naserkorosh1986@gmail.com>
2021-08-04 23:34:43 +0430

    add ralated products components

---
+++ src/Components/relatedProducts/RelatedProducts.jsx
@@ -0,0 +1,104 @@
+import React from 'react';
+
+export default function RelatedProducts() {
+    return (
+       
+<section class="container text-center">
+<h4 class="my-4">محصولات مرتبط</h4>
+  <div class="row">
+
+    <div class="col-md-6 col-lg-3 mb-5">
+
+      <div class="">
+
+        <div class="view zoom overlay z-depth-2 rounded shadow">
+          <img class="img-fluid rounded w-100"
+            src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/12a.jpg" alt="Sample"/>
+        
+        </div>
+
+        <div class="pt-4">
+
+          <h5>بلوز مردانه</h5>
+          <h6>12.99 $</h6>
+        </div>
+
+      </div>
+
+    </div>
+
+    <div class="col-md-6 col-lg-3 mb-5">
+
+      <div class="">
+
+        <div class="view zoom overlay z-depth-2 rounded shadow">
+          <img class="img-fluid rounded w-100"
+            src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/13a.jpg" alt="Sample"/>
+        
+        </div>
+
+        <div class="pt-4">
+
+          <h5>بلوز مردانه</h5>
+          <h6>12.99 $</h6>
+
+        </div>
+
+      </div>
+
+    </div>
+
+    <div class="col-md-6 col-lg-3 mb-5">
+
+      <div class="">
+
+        <div class="view zoom overlay z-depth-2 rounded shadow">
+          <img class="img-fluid rounded w-100"
+            src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/14a.jpg" alt="Sample"/>
+         
+         
+        </div>
+
+        <div class="pt-4">
+
+          <h5>بلوز مردانه</h5>
+          <h6>
+            <span class="text-danger mr-1">$12.99</span>
+            <span class="text-grey"><s>$36.99</s></span>
+          </h6>
+
+        </div>
+
+      </div>
+
+    </div>
+
+    <div class="col-md-6 col-lg-3 mb-5">
+
+      <div class="">
+
+        <div class="view zoom overlay z-depth-2 rounded shadow">
+          <img class="img-fluid rounded w-100"
+            src="https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/Vertical/15a.jpg" alt="Sample"/>
+          
+        </div>
+
+        <div class="pt-4">
+
+          <h5>بلوز مردانه</h5>
+          <h6>
+            <span class="text-danger mr-1">$12.99</span>
+            <span class="text-grey"><s>$36.99</s></span>
+          </h6>
+
+        </div>
+
+      </div>
+
+    </div>
+
+  </div>
+
+</section>
+    )
+}
